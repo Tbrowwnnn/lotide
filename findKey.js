03065827fd86391b7d9887ccd31ad2e4384cc623
Tyler Brown <tyler.brown@telus.com>
2022-11-17 14:37:42 -0700

    adding find key to lotide

---
+++ findKey.js
@@ -0,0 +1,37 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed:  ${actual}  ===   ${expected}`);
+  } else (console.log(`❌❌❌ Assertion Failed: ${actual}  !==   ${expected}`));
+};
+
+const gameList = {
+  "Persona 5": { score: 98 },
+  "Breath of the Wild": { score: 99 },
+  "Red Dead Redemption 2": { score: 95 },
+  "The Witcher 3": { score: 100 },
+  "Final Fantasy X": { score: 90 },
+  "Ghost of Tsushima": { score: 95 },
+  "Yakuza Kiwami 2": { score: 86 }
+};
+
+const findKey = function(objectList, condition) {
+
+  for (const games in objectList) {
+    const gameList = objectList[games];
+
+    for (const scores in gameList) {
+
+      if (condition(gameList)) {
+        return games;
+
+      }
+    }
+  }
+};
+
+const gameScore = findKey(gameList, x => x.score === 95);
+
+assertEqual(gameScore, "Red Dead Redemption 2");
+
+console.log(gameScore);
+
