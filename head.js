8a2f2aa9caa534622f00fe6b41d1183e4aba403a
Tyler Brown <tyler.brown@telus.com>
2022-11-21 08:55:36 -0700

    refactoring head.js

--- head.js
+++ head.js
@@ -1,11 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed:  ${actual}  ===   ${expected}`);
-  } else (console.log(`❌❌❌ Assertion Failed: ${actual}  !==   ${expected}`));
-};
-
-//assertEqual("Lighthouse Labs", "Bootcamp");
-//assertEqual(1, 1);
+const assertEqual = require('./assertEqual');
 
 const head = function(array) {
   for (let i = 0; i < 1; i++) {
