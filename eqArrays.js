7815a1a984502b5a4ad4a0de53d92f0726efe95e
Tyler Brown <tyler.brown@telus.com>
2022-11-21 09:25:33 -0700

    Updating lotide for refactoring, assertArraysEqual

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,4 @@
 
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed:  ${actual}  ===   ${expected}`);
-  } else (console.log(`❌❌❌ Assertion Failed: ${actual}  !==   ${expected}`));
-};
-
 let eqArrays = function(array1, array2) {
 
   if (array1.length !== array2.length) {
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,9 @@
+const assertArraysEqual = require('../assertArraysEqual');
+const eqArrays = require('../eqArrays');
+
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+assertArraysEqual(eqArrays(["1", "2", "3", "4"], ["1", "2", 3])) // => false
