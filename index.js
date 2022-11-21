74976044966bf12ce625e3c7ef2661136ad39ddc
Tyler Brown <tyler.brown@telus.com>
2022-11-21 14:03:03 -0700

    deleted no longer used file

---
+++ index.js
@@ -0,0 +1,40 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPositions = letterPositions('./letterPositions');
+const map = map('./map');
+const pig_latin = pig_latin('./pig-latin');
+const reverse = reverse('./reverse');
+const takeUntil = takeUntil('./takeUntil');
+const without = wtihout('./without');
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  countLetters,
+  countOnly,
+  eqArrays,
+  assertArraysEqual,
+  assertObjectsEqual,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  map,
+  pig_latin,
+  reverse, 
+  takeUntil,
+  without
+};
